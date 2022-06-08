2164d5dfedb9ab97b0040cf6176a3d9460875ac7
Toragdar <Toragdar@gmail.com>
2022-06-08 21:59:10 +0300

    -

--- Assets/Resources/Script/Enemy/EnemySpawner.cs
+++ Assets/Resources/Script/Enemy/EnemySpawner.cs
@@ -3,13 +3,9 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-	[SerializeField]
-	SOActorModel actorModel;
-	[SerializeField]
-	float spawnRate;
-	[SerializeField]
-	[Range(0, 10)]
-	int quantity;
+	[SerializeField] SOActorModel actorModel;
+	[SerializeField] float spawnRate;
+	[SerializeField] [Range(0, 10)] int quantity;
 	GameObject enemies;
 
 	void Awake()
@@ -38,42 +34,3 @@ public class EnemySpawner : MonoBehaviour
 		return enemy;
 	}
 }
-
-
-
-//using System.Collections;
-//using UnityEngine;
-//
-//public class EnemySpawner : MonoBehaviour
-//{
-//    [SerializeField] SOActorModel actorModel;
-//    [SerializeField] float spawnRate;
-//    [SerializeField] [Range(0,10)]
-//
-//    int quantity;
-//    GameObject enemies;
-//
-//    private void Awake()
-//    {
-//        enemies = GameObject.Find("_Enemies");
-//        StartCoroutine(FireEnemy(quantity, spawnRate));
-//    }
-//    IEnumerator FireEnemy(int qnt, float spwnRate)
-//    {
-//        for(int i = 0; i < qnt; i++)
-//        {
-//            GameObject enemyUnit = CreateEnemy();
-//            enemyUnit.gameObject.transform.SetParent(this.transform);
-//            enemyUnit.transform.position = transform.position;
-//            yield return new WaitForSeconds(spwnRate);
-//        }
-//        yield return null;
-//    }
-//    GameObject CreateEnemy()
-//    {
-//        GameObject enemy = GameObject.Instantiate(actorModel.actor) as GameObject;
-//        enemy.GetComponent<IActorTemplate>().ActorStats(actorModel);
-//        enemy.name = actorModel.actorName.ToString();
-//        return enemy;
-//    }
-//}
